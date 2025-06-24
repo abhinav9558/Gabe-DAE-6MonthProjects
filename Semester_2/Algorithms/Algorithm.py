4d32d3acf0dade9435d083cc3ee7ae148d8580bb
AntoTheSol <AntonioSolomon1776@gmail.com>
2025-06-24 14:32:56 -0400

    Created my index.md that will be used for my GitHub pages

--- Semester_2/Algorithms/Algorithm.py
+++ Semester_2/Algorithms/Algorithm.py
@@ -4,4 +4,8 @@ original_list = [7, 2, 9, 4, 1, 8, 5, 10, 3, 6]
 
 def sort_num():
 
-    
+    if original_list[i] > original_list[i + 1]:
+
+        original_list[i] = original_list[i + 1]
+
+        
